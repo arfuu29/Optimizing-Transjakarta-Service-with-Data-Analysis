329a8c36c17c344dde677d100b3439c9bdbaaafb
Fadhilah Arfu <106544070+arfuu29@users.noreply.github.com>
2025-07-20 01:03:01 +0700

    Add files via upload

---
+++ Transjakarta_dashboard_link.py
@@ -0,0 +1,4 @@
+# Link Tableau Public
+tableau_url = "https://public.tableau.com/views/TransJakartaDashboard_17528290483210/DashboiardOverview?:language=en-GB&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
+
+print("Lihat dashboard Tableau Public di:", tableau_url)
